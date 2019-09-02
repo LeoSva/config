135f5a42cd96b6a0bfb3a71e28ac93a1ee6a5c33
Leonaldo <s2it_lsilva@uolinc.com>
2019-09-02 19:54:39 -0300

    removida segurança

--- configClient/src/main/java/com/leo/poc/configClient/ConfigClientApp.java
+++ configClient/src/main/java/com/leo/poc/configClient/ConfigClientApp.java
@@ -1,13 +1,26 @@
 package com.leo.poc.configClient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ConfigClientApp {
 	
 	public static void main(String[] args) {
         SpringApplication.run(ConfigClientApp.class, args);
     }
+	
+	@Value("${arquivo}")
+	private String param;
+	
+	@GetMapping("/config/{usuario}")
+	public String getConfigByUser(@PathVariable("usuario") String usuario ) {
+		return String.format("Olá! %s, arquivo atual: %s", usuario, param);
+	}
 	
 }
